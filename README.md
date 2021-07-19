f7f88644a5397df831e26e3a241bb4b03c298a1a
stevending1st <1139274654@qq.com>
2021-07-19 23:32:13 +0800

    init finish

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 
 [Zero - 折染](https://stevending1st.github.io/zero/) 是一个以折叠和染色为核心的游戏。
 
+这个项目是为了参与 <a href="https://jike.ruguoapp.com/"  target="_blank">即刻</a> 的 **JitHub** 圈子发起的 **Hackathon** 而开发的。
+
+共有 64（8 * 8）个方格，每一个方格有 4 个着色方案，有 4 关，暗含 **1024**（64 * 4 * 4）。
+
+
 ## 玩法
 
 折染是一个以折叠和染色为核心的游戏（下面用数值表示颜色深浅，数值越大颜色越深 0 为无色）。折是指以画纸的某一条轴对折，重叠了两个方格按照如下方式重新染色：当两个方格原本都没有颜色，则折叠后两个方格依然没有颜色；其他的情况，两个方格的颜色总和，如果超过阈值则减去阈值；如果不超过阈值则为之前所求的和，特别地，两个方格颜色是阈值，则颜色变成最浅。目前默认阈值是4。
@@ -13,3 +18,14 @@
 通关条件：当<span style="color: red;">红色</span>标记的方格的颜色染成对应值时，则通关。
 
 共有 64 个方格，每一个方格有 4 个着色方案，有 4 关，暗含 1024（64 * 4 * 4）。
+
+
+## 致谢
+
+感谢即友 <a href="https://web.okjike.com/u/40780FE7-168D-489B-8AAE-5894AA81B346"  target="_blank">杰克有茶</a> 发起本次活动 🍉
+
+感谢即友 <a href="https://web.okjike.com/u/105ad022-f646-48c0-8236-6007ee5179c5"  target="_blank">苦愚君</a> 的珍贵箴言 🍗
+
+## 广而告之
+
+欢迎各位点击👉 <a href="https://m.okjike.com/download?s=eyJ1IjoiNWZkYzMwZTNhNzJiZGUwMDE3YzA1NDAwIn0%3D"  target="_blank">链接</a>，下载 <a href="https://m.okjike.com/download?s=eyJ1IjoiNWZkYzMwZTNhNzJiZGUwMDE3YzA1NDAwIn0%3D"  target="_blank">即刻 APP</a>
--- src/index.js
+++ src/index.js
@@ -1,66 +1,143 @@
-const { fold } = require("./fold");
-
-let test = [
-  [1, 2, 3, 4, 4, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [4, 3, 2, 1, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 2, 3, 4, 0, 0, 0, 0],
-];
-
-let test1 = [
-  [0, 0, 0, 3, 0, 0, 0, 0],
-  [0, 0, 3, 0, 0, 0, 0, 0],
-  [0, 3, 3, 3, 0, 0, 0, 0],
-  [3, 3, 3, 3, 0, 0, 0, 0],
-  [0, 0, 0, 0, 3, 3, 3, 0],
-  [0, 0, 0, 0, 3, 3, 3, 0],
-  [0, 0, 0, 0, 0, 0, 3, 0],
-  [0, 0, 0, 0, 0, 0, 0, 3],
-];
-// [6,2] 3
+    //localStorage.setItem("checkpointNow", 2);
+    //localStorage.setItem("checkpointNow", checkpointNow);
+    //localStorage.getItem("checkpointNow");
 
 
+    // 初始化关卡
+    let checkpointNow = localStorage.getItem("checkpointNow") ? Number(localStorage.getItem("checkpointNow")) : 0;
 
-let test2 = [
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 2, 0, 0, 0, 0, 0],
-  [0, 0, 2, 3, 0, 0, 2, 3],
-  [0, 0, 3, 0, 0, 0, 3, 0],
-  [0, 0, 0, 3, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [3, 2, 0, 0, 3, 0, 0, 0],
-  [0, 0, 3, 0, 0, 0, 3, 0],
-];
-// [0, 5] 3
+    // 初始化步幅记录（最后 5 步）
+    let lastFiveStep = [];
 
-let test3 = [
-  [0, 0, 0, 4, 3, 0, 0, 0],
-  [0, 0, 4, 0, 0, 3, 0, 0],
-  [0, 4, 0, 0, 0, 0, 3, 0],
-  [4, 0, 0, 0, 0, 0, 3, 0],
-  [4, 2, 2, 2, 3, 3, 0, 0],
-  [0, 4, 2, 2, 3, 3, 0, 0],
-  [0, 0, 4, 2, 3, 0, 0, 0],
-  [0, 0, 0, 4, 0, 0, 0, 0],
-];
-// [6, 6] 3
+    // 更新步幅
+    function pushLastFiveStep(arr){
+      if(lastFiveStep.length >= 6){
+        lastFiveStep.shift();
+        lastFiveStep.push(arr);
+      } else {
+        lastFiveStep.push(arr);
+      }
+    }
 
-let test4 = [
-  [0, 0, 0, 0, 0, 0, 0, 3],
-  [0, 0, 0, 3, 0, 0, 0, 0],
-  [3, 0, 0, 0, 0, 3, 0, 0],
-  [0, 3, 0, 3, 0, 0, 0, 0],
-  [3, 0, 3, 0, 3, 0, 0, 0],
-  [0, 0, 0, 3, 0, 0, 0, 0],
-  [3, 0, 0, 0, 0, 0, 3, 0],
-  [0, 0, 0, 3, 0, 0, 0, 0],
-];
+    // 后退步幅
+    function popLastFiveStep(){
+      if(lastFiveStep.length <= 1){
+      } else {
+        lastFiveStep.pop();
+      }
+    }
 
-// [5, 5] 3
+    // 获取渲染位置
+    let chessboardBox = document.getElementById("chessboard");
 
-console.log(fold(test, [6, 0]));
+    function thisRender(round){
+      chessboardBox.innerHTML = rederRow(round);
+      checks[checkpointNow].map(items => {
+        const {coordinate, value} = items;
+        const answerCell = document.getElementById(`cell-${coordinate[0]}-${coordinate[1]}`);
+        answerCell.style.color = "red";
+        answerCell.innerText = value;
+      })
+    }
 
+    // 将当前关卡初始数据存储、渲染
+    pushLastFiveStep(rounds[checkpointNow]);
+    thisRender(lastFiveStep[lastFiveStep.length - 1]);
+
+    // 折
+    function thisfold(arrs, axis){
+      let thisstep = fold(arrs, axis);
+      if (isWin(thisstep, checks[checkpointNow])) {
+        pushLastFiveStep(thisstep);
+        thisRender(lastFiveStep[lastFiveStep.length - 1]);
+        openDialogBox();
+      } else {
+        pushLastFiveStep(thisstep);
+        thisRender(lastFiveStep[lastFiveStep.length - 1]);
+      }
+    }
+
+    // 后退
+    function back(){
+      popLastFiveStep();
+      thisRender(lastFiveStep[lastFiveStep.length - 1]);
+    }
+
+    // 重新开始
+    function Restart(){
+      pushLastFiveStep(rounds[checkpointNow]);
+      thisRender(lastFiveStep[lastFiveStep.length - 1]);
+    }
+
+    function openDialogBox(){
+      document.getElementById("comeonBox").innerText = checkpointNow >= 3 ? "您已经通关了！": `您已经通过第 ${checkpointNow + 1} 关`;
+      let button = document.getElementById("dialogBoxButton");
+      button.innerText = checkpointNow >= 3 ? "关闭" : "下一关";
+      document.getElementById('dialogBoxBackground').style.display = "flex";
+      takeScreenshot();
+    }
+
+    function closeDialogBox(){
+      document.getElementById('dialogBoxBackground').style.display = "none";
+    }
+
+    // 下一关
+    function next(){
+      if(checkpointNow < 3) {
+        checkpointNow++
+        localStorage.setItem("checkpointNow", checkpointNow);
+      } else {
+        checkpointNow = 0;
+        localStorage.setItem("checkpointNow", checkpointNow);
+      }
+      lastFiveStep = [];
+      // 将当前关卡初始数据存储、渲染
+      pushLastFiveStep(rounds[checkpointNow]);
+      thisRender(lastFiveStep[lastFiveStep.length - 1]);
+      closeDialogBox();
+    }
+
+    let ruleTable = document.getElementById("ruleTable");
+    ruleTable.innerHTML = rederRow(roundlearn);
+
+    let menuList = ["root", "rule", "about"]
+    function clickMenu(showItems) {
+      menuList.map(items => {
+        if(items === showItems){
+          document.getElementById(items).style.display = "block";
+        } else {
+          document.getElementById(items).style.display = "none";
+        }
+      })
+    }
+
+    function takeScreenshot() {
+      console.log('test');
+      let chessboard = document.getElementById('chessboard');
+      chessboard.style.border = "1px solid #333";
+      let chessboardList = chessboard.childNodes;
+      console.log(chessboardList);
+      // chessboardList.map((items,index) => {
+      //   console.log("index:", index, ",items:", items)
+      // });
+      for(let i = 0; i < chessboardList.length; i++){
+        console.log("index:", i, ",items:", chessboardList[i])
+        chessboardList[i].style.border = "none";
+        let cCCNode = chessboardList[i].childNodes;
+        for(let j = 0; j < cCCNode.length; j++){
+          cCCNode[j].style.border = "none";
+          cCCNode[j].innerText = "";
+        }
+      }
+      // console.log(chessboard);
+      new html2canvas(chessboard)
+        .then(canvas => {
+          let oImg = new Image();
+          oImg.style.width = "16rem";
+          oImg.style.height = "16rem";
+          oImg.src = canvas.toDataURL(); 
+          let outImg = document.getElementById("outImg");
+          outImg.innerHTML = '';
+          outImg.appendChild(oImg);
+        });
+    }
